90cbe401d111b3afe6e00b232cbf12b7534fd414
Alexander Repeta <alexander.repeta@gmail.com>
2021-10-19 19:24:02 +0300

    add examples

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { Switch, Route, NavLink } from 'react-router-dom';
-import { TestPage } from 'pages/TestPage';
 import { HomePage } from 'pages/HomePage';
 
 import { PageContainer } from 'components/PageContainer/PageContainer';
@@ -10,31 +9,16 @@ const StyledNavLink = styled(NavLink)`
   margin: 0 10px;
 `;
 
-const routes = {
-  testPage: '/test-page',
-};
-
 export const App = () => {
   return (
     <PageContainer>
       <nav>
         <StyledNavLink to="/">home page</StyledNavLink>
-        <StyledNavLink
-          to={{
-            pathname: routes.testPage,
-            state: { from: '/?sortBy=asc' },
-          }}
-        >
-          test page
-        </StyledNavLink>
       </nav>
 
       <Switch>
         <Route path="/" exact>
           <HomePage />
-        </Route>
-        <Route path={routes.testPage}>
-          <TestPage />
         </Route>
       </Switch>
     </PageContainer>
