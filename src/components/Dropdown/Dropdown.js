f07c6eaf4ba3d91f9b0a6aac99377aa760b88c18
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-05-06 12:21:45 +0200

    Dropdowh. 1 button and 2

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -7,6 +7,15 @@ class Dropdown extends React.Component {
   state = {
       visible: false,
   }
+
+ // Cоздаём на 1 кнопке переключение. условие что toggle оббратное от того что есть
+
+ toggle = () => {this.setState (prevState => ({
+  visible: !prevState.visible,
+ }))}
+
+
+  // // Cоздаём на 2 кнопках переключение
   // При нажатии менять будем Dropdown на видно
   show = () => {
       this.setState({visible: true})
@@ -19,8 +28,15 @@ class Dropdown extends React.Component {
   render() {
     return (
       <div className="Dropdown">
+     {/* Переключатель на 1 кнопке. Когда скрыто - надпись "Показать", когда показывает - надпись скрыть */}
+<button type="button" onClick={this.toggle}>{this.state.visible ? "Скрыть" : "Показать"}</button>
+
+ {/* Переключатель на 2 кнопках */}
+
         <button type="button" onClick={this.show}>Показать Dropdown</button>
         <button type="button" onClick={this.hide}>Скрыть Dropdown</button>
+
+ 
         {this.state.visible && (
           <div style={{ border: '1px solid black', padding: '16px' }}>
             Тело Dropdown 
